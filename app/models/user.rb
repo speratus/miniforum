cb8c4af7dbc1373d9166cd11380e441b1e9850e2
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-05-25 16:11:40 -0400

    adds user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :topics
     has_many :comments, class_name: "Reply"
     has_many :likes
+
+    validates :email, :name, :username, :password, presence: true
+    validates :username, :email, uniqueness: true
+    validates :username, format: {with: /\w/, message: "only alphanumeric characters allowed"}
+    validates :email, format: {with: /[\w\.\+]+@\w+(?:\.\w+)+/, message: "must be a valid email address"}
 end
