17dd085964fd3941a7f0a0cb8ef2eaf3894b36a0
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-01 16:21:14 -0400

    adds validations tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,54 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Saves valid objects" do
+    user = User.new(
+      username: "jon",
+      name: "Jon doe",
+      email: "hello@world.com",
+      password: "nobody knows"
+    )
+
+    assert_equal true, user.save
+  end
+
+  test "Will not save a user missing an attribute" do
+    user = User.new(
+      username: 'jon',
+      email: 'hello@world.com',
+      password: 'nobody knows'
+    )
+    assert_equal false, user.save
+  end
+
+  test "will not save a user with an invalid username" do
+    user = User.new(
+      username: 'jon++++. cool',
+      name: 'jon doe',
+      email: 'hello@world.com',
+      password: 'nobody knows'
+    )
+    assert_equal false, user.save
+  end
+
+  test "will not save a user with an invalid email address" do
+    user = User.new(
+      username: 'jon',
+      name: 'jon doe',
+      email: 'hello&world@universe.co',
+      password: 'nobody knows'
+    )
+    assert_equal false, user.save
+  end
+
+  test "email must be unique" do
+    user = User.new(
+      username: 'jon',
+      name: 'jon doe',
+      email: 'MyString',
+      password: 'nobody knows'
+    )
+    assert_equal false, user.save
+  end
 end
