c7c88edb4f483d5d3f6661523a572fdc3322d59b
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-01 15:24:18 -0400

    adds topic validations tests

--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -4,4 +4,22 @@ class TopicTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "saves valid objects" do
+    user = users(:one)
+    topic = Topic.new(user: user, content: "Hello World", title: "A Test!")
+    assert_equal true, topic.save
+  end
+
+  test "Does not save invalid objects" do
+    topic1 = Topic.new
+    assert_equal false, topic1.save
+
+    topic2 = Topic.new(title: "A Test!", content: "Hello World")
+    assert_equal false, topic2.save
+
+    user = users(:one)
+    topic3 = Topic.new(title: "A Test", user: user)
+    assert_equal false, topic3.save
+  end
 end
