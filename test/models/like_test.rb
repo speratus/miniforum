babe5be654090b598409682f54dcae320f846990
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-01 12:20:23 -0400

    adds like validation tests and fixes users fixtures

--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -4,4 +4,30 @@ class LikeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "fixtures are functioning" do
+    assert_equal 2, likes(:one, :two).length
+  end
+
+  test "correctly validates presence of user and post" do
+    reply_one = replies(:one)
+    user_one = users(:one)
+    like = Like.new(post: reply_one, user: user_one)
+    assert_silent do
+      like.save
+    end
+  end
+
+  test "validations fail without user or post" do
+    like1 = Like.new
+    assert_equal false, like1.save
+
+    topic_one = topics(:one)
+    like2 = Like.new(post: topic_one)
+    assert_equal false, like2.save
+
+    user_one = users(:one)
+    like3 = Like.new(user: user_one)
+    assert_equal false, like3.save
+  end
 end
