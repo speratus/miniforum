6e9ae5fa9cdc195ef42a9ad7fee0fa08dd3de197
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-02 11:27:17 -0400

    adds topic association tests

--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -22,4 +22,20 @@ class TopicTest < ActiveSupport::TestCase
     topic3 = Topic.new(title: "A Test", user: user)
     assert_equal false, topic3.save
   end
+
+  test "Belongs to a user" do
+    user = users(:topic_user)
+    topic = topics(:associations)
+    assert_equal user, topic.user
+  end
+
+  test "has many replies" do
+    topic = topics(:associations)
+    assert_equal 1, topic.replies.length
+  end
+
+  test "has many likes" do
+    topic = topics(:associations)
+    assert_equal 1, topic.likes.length
+  end
 end
