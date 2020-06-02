60bcd7d014f8b1513d3c63b1228a1d0efbabac37
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-02 11:18:08 -0400

    adds missing association for reply

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,6 +2,7 @@ class Reply < ApplicationRecord
   belongs_to :user
   belongs_to :post, polymorphic: true
   has_many :replies, as: :post
+  has_many :likes, as: :post
 
   validates :content, :user, :post, presence: true
 end
