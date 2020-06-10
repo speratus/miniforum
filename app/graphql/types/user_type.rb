e6206bb2d38756db191db8bcbdc1efd52810fc8b
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-10 16:21:46 -0400

    generates user graphql type

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,11 @@
+module Types
+  class UserType < Types::BaseObject
+    field :username, String, null: true
+    field :name, String, null: true
+    field :email, String, null: true
+    field :id, Integer, null: true
+    field :comments, [Types::ReplyType], null: true
+    field :likes, [Types::LikeType], null: true
+    field :topics, [Types::TopicType], null: true
+  end
+end
