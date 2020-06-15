8d4d20c8a35df2073486af502db31495f576c216
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-15 17:06:11 -0400

    fills out users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.new(user_params)
+
+        if user.save
+            render json: user_json(user)
+        else
+            render json: {
+                message: 'Failed to create user',
+                errors: user.errors.full_messages
+            }
+        end
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    end
+
+    def user_json(user)
+        user.as_json(only: [:id, :name, :username, :email])
+    end
 end
