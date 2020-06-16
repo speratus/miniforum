d08eb6a810db0b9be206ee2b4869e0f09775fbc4
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-16 11:12:55 -0400

    generates and fills out create_topic mutation

---
+++ app/graphql/mutations/create_topic.rb
@@ -0,0 +1,24 @@
+module Mutations
+  class CreateTopic < BaseMutation
+    field :errors, [String], null: false
+    field :topic, Types::TopicType, null: true
+
+    argument :user_id, ID, required: true
+    argument :title, String, required: true
+    argument :content, String, required: true
+
+
+    def resolve(title:, content:, user_id:)
+      topic = Topic.new(title: title, content: content, user_id: user_id)
+      if topic.save
+        topic: topic,
+        errors: []
+      else
+        {
+          topic: nil,
+          errors: topic.errors.full_messages
+        }
+      end
+    end
+  end
+end
