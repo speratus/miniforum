c450a5d7e72b5959bdfead28ee30636b5dd0c1b9
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-16 10:05:29 -0400

    adds create to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(username: session_params[:username])
+        if user && user.authenticate(session_params[:password])
+            token = JWT.encode({user_id: user.id}, ENV['JWT_SECRET'])
+            render json: {token: token, user_id: user.id}
+        else
+            render json: {message: "Incorrect username or password"}
+        end
+    end
+
+    private
+
+    def session_params
+        params.require(:session).permit(:username, :password)
+    end
 end
