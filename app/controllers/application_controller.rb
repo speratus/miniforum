a4840f617d6e9459a4f39eb57cf2905535c3d65d
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-16 09:56:11 -0400

    adds current_user in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+class AuthenticationError < StandardError
+
+end
+
 class ApplicationController < ActionController::API
+    def current_user
+        token = request.headers['Access-Token']
+        raise AuthenticationError if token.nil?
+        user_id = JWT.decode(token ENV['JWT_SECRET'])[0]['user_id']
+        @user = User.find_by(id: user_id)
+    end
 end
