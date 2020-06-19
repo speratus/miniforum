4f3d8175bb0f8f0cb4299ac31c91d7280b436096
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-19 11:58:39 -0400

    overrides resolve_type method

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,5 +1,9 @@
 module Types
   class PostType < Types::BaseUnion
     possible_types Types::ReplyType, Types::TopicType
+
+    def self.resolve_type(object, context)
+      object.graphql_type
+    end
   end
 end
